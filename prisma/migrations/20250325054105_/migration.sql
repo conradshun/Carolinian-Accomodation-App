65808dbaa3bbbc2cbae6a35941f1df4b792d21ce
Concyzxc <conradmativo@gmail.com>
2025-03-26 21:30:52 +0800

    minor changes to the user table and added an 'updated_at' variable

--- prisma/migrations/20250325054105_/migration.sql
+++ prisma/migrations/20250325054105_/migration.sql
@@ -1,11 +1,12 @@
--- CreateTable
+-- Users table
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `users_email_key`(`email`),    PRIMARY KEY (`id`)
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
